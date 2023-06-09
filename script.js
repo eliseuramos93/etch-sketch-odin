e1851fbeabe47e8bb858817f462f67c43a5b051e
eliseuramos93 <eliseu.mec@gmail.com>
2023-06-09 13:57:10 -0300

    Add rule to only execute painting while mousedown

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /*==============================================================================
 1 [OK] create a webpage with a 16x16 of square grids
   1.1 [OK] create the divs using javascript
-2 [ ] set up a hover effect that the grid divs change color when the mouse passes
-over them
-3 [ ] add a button to the top of the screen that will send the user a popup asking
-for the number of squares per side for the new grid
+2 [ ] set up a hover effect that the grid divs change color when the mouse 
+passes over them
+3 [ ] add a button to the top of the screen that will send the user a popup 
+asking for the number of squares per side for the new grid
   3.1 [ ] set the maximum value to 100
 4 [ ] create a rainbow option (random rgb color after each pass)
 5 [ ] create a 10% increment per pass rule to create a fade-in effect
@@ -12,11 +12,11 @@ for the number of squares per side for the new grid
 
 /* == 1 == 
 
-* save the #canvas reference into a variable;
-* create a variable with the grid size
-* create a variable for the grid column and grid row
-* create the necessary amount of rows in each column
-* create the necessary amount of columns in the canvas 
+[OK] save the #canvas reference into a variable;
+[OK] create a variable with the grid size
+[OK] create a variable for the grid column and grid row
+[OK] create the necessary amount of rows in each column
+[OK] create the necessary amount of columns in the canvas 
 
 */
 
@@ -38,7 +38,7 @@ function createGrid(gridSize) {
     column = document.createElement('div');
     column.classList.add('column', `index${index}`);
     createColumn(gridSize);
-    canvas.appendChild(column)
+    canvas.appendChild(column);
   }
 }
 
@@ -46,24 +46,33 @@ createGrid(gridSize);
 
 /* == 2 == 
 
-* capture the moment when the mouse is over each individual block;
-* set up a variable to determine if the left-button is being pressed
-* set up a trigger for changing the background-color on mouseover w/ mousedown
+[OK] capture the moment when the mouse is over each individual block;
+[OK] set up a variable to determine if the left-button is being pressed
+[OK] set up a trigger for changing the background-color on mouseover w/ mousedown
 
 => stack-overflow ref 4 mouse is down:
 https://stackoverflow.com/questions/47641309/combine-mouse-events-in-javascript
 
 */
 
-function paintBlack(element) {
+let mouseIsDown = false;
+window.addEventListener('mousedown', () => {mouseIsDown = true});
+window.addEventListener('mouseup', () => {mouseIsDown = false})
+
+function changeColor(element, color) {
+  if (mouseIsDown){
+    element.setAttribute('style', `background-color: ${color}`);
+  }
+}
+
+function paintGrid(element) {
     element.addEventListener('mouseover', () => {
-      element.setAttribute('style', 'background-color: #000000');
-      console.log(element);
+      changeColor(element, 'black');
     });
 }
 
 const columns = document.querySelectorAll('.column');
 columns.forEach(column => {
   let rows = column.querySelectorAll('.row');
-  rows.forEach(row => paintBlack(row));
+  rows.forEach(row => paintGrid(row));
 });
